0bcda98673f984a92f1cec9918a93e94052a74d9
Agney Bhople <49913416+coodcoder981@users.noreply.github.com>
2021-04-22 14:10:31 +0530

    Add files via upload

---
+++ simplemath.go
@@ -0,0 +1,22 @@
+package utility
+
+// This is simple calculator that basically does simple math operations
+
+func Add(a, b int) int {
+
+	return a + b
+}
+
+func Subtract(a, b int) int {
+
+	return a - b
+}
+
+func divide(a, b int) int {
+
+	return a / b
+}
+
+func multiple(a, b int) int {
+	return a / b
+}
